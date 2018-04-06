2661a5f3c5345e4f7ba06307f4fa0d205b457010
Batgurl <ltbelay@gmail.com>
2018-04-06 11:35:30 -0700

    assignment3

---
+++ js/01-functions-practice.js
@@ -0,0 +1,16 @@
+//STEP 1
+
+function halfOf(window.prompt("Enter a number")) {
+    var half = num / 2;
+    window.document.write('Half of ' + num + ' is ' +  half);
+    return half;
+}
+
+halfOf(5);
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
